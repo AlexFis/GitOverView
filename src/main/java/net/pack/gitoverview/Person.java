9b09e2d5c20a4b5e5321ca6659efb2ba85e9b5b5
AlexFis <ofisenko341@gmail.com>
2019-10-06 21:29:32 +0300

    Adding class Person

---
+++ src/main/java/net/pack/gitoverview/Person.java
@@ -0,0 +1,30 @@
+package net.pack.gitoverview;
+
+public class Person {
+    private String firstName;
+    private String secondName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
+    }
+}
